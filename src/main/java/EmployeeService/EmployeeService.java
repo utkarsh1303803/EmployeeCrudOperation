7616e4c02f800c5088c705772cc05823df428109
Utkarsh Singh <singhutkarsh3637@gmail.com>
2026-06-22 14:52:39 +0530

    Added EmployeeController and EmployeeService

---
+++ src/main/java/EmployeeService/EmployeeService.java
@@ -0,0 +1,14 @@
+package EmployeeService;
+
+import java.util.List;
+
+import entities.Employee;
+
+public interface EmployeeService {
+   
+	List<Employee> getAllemployees();
+	Employee getEmployeeById(Long id);
+    Employee updateEmployee(Employee employee);
+    void deleteEmployee(Long id);
+    Employee saveEmployee(Employee employee);
+ }
--- src/main/java/entities/Employee.java
+++ src/main/java/entities/Employee.java
@@ -1,7 +1,6 @@
 package entities;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -16,109 +15,108 @@ public class Employee {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
 
+    @Column(nullable = false)
     private String firstName;
+
+    @Column(nullable = false)
     private String lastName;
 
     @Column(unique = true, nullable = false)
     private String email;
 
     private String department;
+
     private Double salary;
+
     private String mobileNo;
 
     private LocalDate dateOfJoining;
-    
 
-	public Employee() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
+    public Employee() {
+    }
 
-	public Employee(int id, String firstName, String lastName, String email, String department, Double salary,
-			String mobileNo, LocalDate dateOfJoining) {
-		super();
-		this.id = id;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.email = email;
-		this.department = department;
-		this.salary = salary;
-		this.mobileNo = mobileNo;
-		this.dateOfJoining = dateOfJoining;
-	}
+    public Employee(String firstName, String lastName, String email,
+                    String department, Double salary,
+                    String mobileNo, LocalDate dateOfJoining) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.department = department;
+        this.salary = salary;
+        this.mobileNo = mobileNo;
+        this.dateOfJoining = dateOfJoining;
+    }
 
-	public int getId() {
-		return id;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public String getFirstName() {
-		return firstName;
-	}
+    public String getFirstName() {
+        return firstName;
+    }
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
-	public String getLastName() {
-		return lastName;
-	}
+    public String getLastName() {
+        return lastName;
+    }
 
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
-	public String getEmail() {
-		return email;
-	}
+    public String getEmail() {
+        return email;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-	public String getDepartment() {
-		return department;
-	}
+    public String getDepartment() {
+        return department;
+    }
 
-	public void setDepartment(String department) {
-		this.department = department;
-	}
+    public void setDepartment(String department) {
+        this.department = department;
+    }
 
-	public Double getSalary() {
-		return salary;
-	}
+    public Double getSalary() {
+        return salary;
+    }
 
-	public void setSalary(Double salary) {
-		this.salary = salary;
-	}
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
 
-	public String getMobileNo() {
-		return mobileNo;
-	}
+    public String getMobileNo() {
+        return mobileNo;
+    }
 
-	public void setMobileNo(String mobileNo) {
-		this.mobileNo = mobileNo;
-	}
+    public void setMobileNo(String mobileNo) {
+        this.mobileNo = mobileNo;
+    }
 
-	public LocalDate getDateOfJoining() {
-		return dateOfJoining;
-	}
+    public LocalDate getDateOfJoining() {
+        return dateOfJoining;
+    }
 
-	public void setDateOfJoining(LocalDate dateOfJoining) {
-		this.dateOfJoining = dateOfJoining;
-	}
+    public void setDateOfJoining(LocalDate dateOfJoining) {
+        this.dateOfJoining = dateOfJoining;
+    }
 
-	@Override
-	public String toString() {
-		return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
-				+ ", department=" + department + ", salary=" + salary + ", mobileNo=" + mobileNo + ", dateOfJoining="
-				+ dateOfJoining + "]";
-	}
-    
-	
+    @Override
+    public String toString() {
+        return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName
+                + ", email=" + email + ", department=" + department + ", salary=" + salary
+                + ", mobileNo=" + mobileNo + ", dateOfJoining=" + dateOfJoining + "]";
+    }
 }
