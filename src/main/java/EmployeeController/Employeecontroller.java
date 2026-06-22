f65d08ed0b0d6545172e49dc849deec3634bdd40
Utkarsh Singh <singhutkarsh3637@gmail.com>
2026-06-22 17:23:38 +0530

    changes to Employeecontroller & EmployeeService

--- src/main/java/EmployeeController/Employeecontroller.java
+++ src/main/java/EmployeeController/Employeecontroller.java
@@ -36,20 +36,20 @@ public class Employeecontroller {
      public String saveEmployee(@ModelAttribute Employee employee)
      {
     	 employeeservice.saveEmployee(employee);
-    	 return "redirect:/employees";
+    	 return "redirect:/employees/fetch";
      }
      
      @PostMapping("/update")
      public String updateEmployee(@ModelAttribute Employee employee)
      {
     	 employeeservice.updateEmployee(employee);
-    	 return "redirect:/employees";
+    	 return "redirect:/employees/fetch";
    }
      @GetMapping("/delete/{id}")
      public String deleteEmployee(@PathVariable Long id)
      {
     	 employeeservice.deleteEmployee(id);
-    	 return "redirect:/employees";
+    	 return "redirect:/employees/fetch";
      }
      
      
--- src/main/java/EmployeeService/EmployeeService.java
+++ src/main/java/EmployeeService/EmployeeService.java
@@ -1,20 +1,17 @@
 package EmployeeService;
 
 import java.util.List;
-
 import entities.Employee;
 
 public interface EmployeeService {
-     
-	 
-	List<Employee> getAllemployees();
-	Employee getEmployeeById(Long id);
-<<<<<<< HEAD
-    Employee updateEmployee(Long id, Double salary);
-    void deleteEmployee(Long id);
-=======
+
+    List<Employee> getAllemployees();
+
+    Employee getEmployeeById(Long id);
+
     Employee updateEmployee(Employee employee);
-    void     deleteEmployee(Long id);
->>>>>>> branch 'master' of https://github.com/utkarsh1303803/EmployeeCrudOperation.git
+
+    void deleteEmployee(Long id);
+
     Employee saveEmployee(Employee employee);
- }
+}
